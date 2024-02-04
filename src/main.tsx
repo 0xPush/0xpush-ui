ac6195438363a64c6f63a22d89e31fe7d2163c4b
kelreel <derodroll@gmail.com>
2024-02-04 21:37:25 +0300

    routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,31 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import "normalize.css"
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "normalize.css";
+import "./index.css";
+import {
+  RouterProvider,
+  createMemoryHistory,
+  createRouter,
+} from "@tanstack/react-router";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+import { routeTree } from "./routeTree.gen";
+
+// Create a new router instance
+// const memoryHistory = createMemoryHistory({
+//   initialEntries: ["/"],
+// });
+
+const router = createRouter({ routeTree });
+
+// Register the router instance for type safety
+declare module "@tanstack/react-router" {
+  interface Register {
+    router: typeof router;
+  }
+}
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,5 +1,12 @@
+import { ChakraProvider, createLocalStorageManager } from "@chakra-ui/react";
+import styled from "@emotion/styled";
 import { createRootRoute, Outlet } from "@tanstack/react-router";
 import React, { Suspense } from "react";
+import { Web3ModalProvider } from "../providers/web3-modal-provider";
+import { theme } from "../theme";
+import { PriceProvider } from "../providers/price-provider";
+import { Header } from "../components/layout/header";
+import { Content } from "../components/layout/content";
 
 const TanStackRouterDevtools =
   process.env.NODE_ENV === "production"
@@ -13,13 +20,30 @@ const TanStackRouterDevtools =
         }))
       );
 
+const AppContainer = styled.div`
+  min-height: 100dvh;
+  display: flex;
+  flex-flow: column nowrap;
+`;
+
+const manager = createLocalStorageManager("color-theme");
+
 export const Route = createRootRoute({
   component: () => (
-    <>
-      <Outlet />
+    <Web3ModalProvider>
+      <ChakraProvider theme={theme} colorModeManager={manager}>
+        <PriceProvider>
+          <AppContainer>
+            <Header />
+            <Content>
+              <Outlet />
+            </Content>
+          </AppContainer>
+        </PriceProvider>
+      </ChakraProvider>
       <Suspense>
         <TanStackRouterDevtools />
       </Suspense>
-    </>
+    </Web3ModalProvider>
   ),
 });
