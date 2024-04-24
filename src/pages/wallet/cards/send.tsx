57958e63cf35ada2df83659884d17aea0df41c78
kelreel <derodroll@gmail.com>
2024-04-25 01:39:27 +0300

    send txs from push

--- src/pages/wallet/cards/send.tsx
+++ src/pages/wallet/cards/send.tsx
@@ -3,12 +3,12 @@ import styled from "@emotion/styled";
 import { getDefaultToken } from "components/token-input";
 import { TokenInput } from "components/token-input/token-select";
 import { StyledInput } from "components/ui/styled-input";
-import { formatEther } from "ethers";
-import { useTokenBalance } from "hooks/use-token-balance";
 import { usePushWalletContext } from "providers/push-wallet-provider";
 import { useEffect, useState } from "react";
 import { TokenOption } from "types/token";
-import { useAccount, useClient } from "wagmi";
+import { Address, erc20Abi, formatUnits, parseUnits } from "viem";
+import { sendTransaction } from "viem/actions";
+import { useAccount, useClient, useSendTransaction, useWriteContract } from "wagmi";
 
 const FormLabel = styled.div`
   display: flex;
@@ -38,6 +38,9 @@ export const Send = () => {
   );
   const [to, setTo] = useState("");
 
+  const { data: hash, sendTransaction, isPending } = useSendTransaction();
+  const { writeContractAsync } = useWriteContract();
+
   useEffect(() => {
     if (address) {
       setTo(address);
@@ -46,6 +49,40 @@ export const Send = () => {
 
   const { colorMode } = useColorMode();
   const bgColor = { light: "white", dark: "whiteAlpha.100" };
+
+  const handleSend = () => {
+    if (token.isNative) {
+      sendTransaction(
+        {
+          to: to as Address,
+          value: parseUnits(amount, token.token.decimals),
+          account,
+        },
+        {
+          onSuccess: () =>
+            toast({
+              title: `${formatUnits(parseUnits(amount, token.token.decimals), token.token.decimals)} ${token.token.symbol} sent. Tx: ${hash}.`,
+              status: "success",
+            }),
+        }
+      );
+    } else {
+      writeContractAsync({
+        abi: erc20Abi,
+        address: token.token.address as Address,
+        functionName: "transfer",
+        args: [to as Address, parseUnits(amount, token.token.decimals)],
+        account,
+      })
+        .then((data) =>
+          toast({
+            title: `Tx sent: ${data}.`,
+            status: "success",
+          })
+        )
+        .catch((e) => console.log(e));
+    }
+  };
 
   return (
     <Stack>
@@ -80,6 +117,8 @@ export const Send = () => {
           w="100%"
           colorScheme="red"
           isLoading={false}
+          isDisabled={false}
+          onClick={handleSend}
           loadingText="Wait..."
           type="submit"
         >
